23c5204a58b4953e7758cdc5fc43c3355f40cd90
awerdich <awerdich@gmail.com>
2021-01-01 13:54:25 +0100

    Added latest model updates.

--- src/echoai_pet_measurements/processing.py
+++ src/echoai_pet_measurements/processing.py
@@ -75,6 +75,49 @@ class Videoconverter:
 
         return subsampled_image_array
 
+    def load_video(self, filename):
+        meta = self.meta_df[self.meta_df.filename == filename]
+        data = None
+        if meta.shape[0] > 0:
+            deltaX = np.abs(meta.deltaX.values[0])
+            deltaY = np.abs(meta.deltaY.values[0])
+            if (0 < deltaX) & (deltaX < 1) & (0 < deltaY) & (deltaY < 1):
+                frame_time = meta.frame_time.values[0] * 1e-3
+                rate = 1 / frame_time
+                file = os.path.join(meta.dir.values[0], filename)
+                try:
+                    with lz4.frame.open(file, 'rb') as fp:
+                        data = np.load(fp)
+                except IOError as err:
+                    print('Cannot open npy file.')
+                    print(err)
+        return data
+
+    def process_data(self, data, deltaX, deltaY, frame_time):
+        output_array = np.zeros(1)
+        error = None
+        if (0 < deltaX) & (deltaX < 1) & (0 < deltaY) & (deltaY < 1):
+            frame_time *= 1e-3
+            rate = 1 / frame_time
+            video_len = data.shape[0] / rate
+            # If the rate is higher, we need more frames
+            if (self.min_video_len < video_len) & (self.min_rate < rate):
+                image_array = self.data2imarray(im_data=data, dx=deltaX, dy=deltaY)
+                output_array = self.subsample_video(image_array=image_array,
+                                                    frame_time=frame_time)
+            else:
+                if self.min_rate >= rate:
+                    print(f'Frame rate is too low: {rate:.2f}s^-1. Skipping.')
+                    error = 'frame_rate'
+                if self.min_video_len >= video_len:
+                    print(f'Video is too short: {video_len:.2f}s. Skipping.')
+                    error = 'video_len'
+        else:
+            print('Meta data invalid for {}. Skipping file.')
+            error = 'deltaXY'
+
+        return error, output_array
+
     def process_video(self, filename):
         meta = self.meta_df[self.meta_df.filename == filename]
         output_array = np.zeros(1)
--- src/echoai_pet_measurements/runCFRModel.py
+++ src/echoai_pet_measurements/runCFRModel.py
@@ -7,23 +7,19 @@ Compile all data as .npy array and expand into memory
 
 import os
 import numpy as np
-import pandas as pd
 
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
-from werdich_cfr.tfutils.TFRprovider import DatasetProvider
-from werdich_cfr.utils.processing import Videoconverter
-from werdich_cfr.tfutils.tfutils import use_gpu_devices
+from echoai_pet_measurements.TFRprovider import DatasetProvider
+from echoai_pet_measurements.processing import Videoconverter
 
 #%% Video selection and model parameters
 
-# GPU parameters
-physical_devices, device_list = use_gpu_devices(gpu_device_string='0,1')
 batch_size = 1
 
 # Project directory
-project_dir = os.path.join(os.environ['HOME'], 'cfrmodel')
+project_dir = os.path.join(os.environ['HOME'], 'data', 'cfrmodel')
 
 # Video selection criteria
 max_frame_time_ms = 33.34 # Maximum frame_time acceptable in ms
@@ -31,16 +27,7 @@ min_rate = 1/max_frame_time_ms*1e3
 min_frames = 40 # Minimum number of frames at min_rate (2 s)
 min_length = max_frame_time_ms*min_frames*1e-3
 
-feature_dict = \
-    {
-    'array': ['image', 'shape'],
-    'float': ['rest_global_mbf', 'stress_global_mbf', 'global_cfr_calc'],
-    'int': ['record'],
-    'features': ['image', 'shape',
-    'rest_global_mbf', 'stress_global_mbf',
-    'global_cfr_calc', 'record']
-    }
-
+# Model parameters
 model_dict = \
     {
     'name': 'model',
@@ -55,7 +42,31 @@ model_dict = \
     'bias_init': tf.keras.initializers.Zeros()
     }
 
-#%% Some small helper functions
+feature_dict = \
+    {
+    'array': ['image', 'shape'],
+    'float': ['rest_global_mbf', 'stress_global_mbf', 'global_cfr_calc'],
+    'int': ['record'],
+    'features': ['image', 'shape',
+    'rest_global_mbf', 'stress_global_mbf',
+    'global_cfr_calc', 'record']
+    }
+
+# Training parameters
+train_dict = {'train_device_list': None,
+              'learning_rate': 0.0001,
+              'augment': False,
+              'train_batch_size': 64,
+              'eval_batch_size': 32,
+              'validation_batches': None,
+              'validation_freq': 1,
+              'n_epochs': 300,
+              'verbose': 1,
+              'meta_dir': None,
+              'train_file_list': None,
+              'eval_file_list': None}
+
+#%% Functions
 
 def get_im_generator(im_array_list):
     """ Yield successive images from list """
@@ -87,46 +98,33 @@ def predict_from_array_list(model, model_dict, feature_dict, array_list, batch_s
     dset = dset.map(lambda x: ({'video': x}, {'score_output': 0}))
     dset = dset.batch(batch_size=batch_size, drop_remainder=False).repeat(count=1)
 
+    n_steps = int(np.ceil(len(array_list) / batch_size))
+
     predict_list = list(np.ndarray.flatten(model.predict(dset, verbose=1, steps=n_steps)))
 
     return predict_list
 
-#%% Load video data into memory and start preprocessing
-video_meta_dict = \
-    {
-    'filename': ['KX0000D1.npy.lz4'],
-    'dir': ['/home/andreas/cfrmodel'],
-    'frame_time': [20.702],
-    'deltaX': [0.03667],
-    'deltaY': [0.03367]
-    }
+def runCFRModel(data, frame_time_ms, deltaX, deltaY):
 
-video_meta_df = pd.DataFrame(video_meta_dict)
-VC = Videoconverter(max_frame_time_ms=max_frame_time_ms, min_frames=min_frames, meta_df=video_meta_df)
-filename = 'KX0000D1.npy.lz4'
+    # Load model from checkpoint
+    checkpoint_file = os.path.join(project_dir, 'checkpoint.h5')
+    model = load_model(checkpoint_file)
 
-image_array_file_list = []
-image_array_list = []
-meta_disqualified_list = []
+    VC = Videoconverter(max_frame_time_ms=max_frame_time_ms, min_frames=min_frames, meta_df=None)
+    error, im = VC.process_data(data=data,
+                                deltaX=deltaX,
+                                deltaY=deltaY,
+                                frame_time=frame_time_ms)
+    predictions=[]
+    image_array_list = []
+    if np.any(im):
+        image_array_list.append((im, np.asarray(im.shape, np.int32)))
+        predictions = predict_from_array_list(model=model,
+                                              model_dict=model_dict,
+                                              feature_dict=feature_dict,
+                                              array_list=image_array_list,
+                                              batch_size=batch_size)
+    else:
+        print(f'This video file does not qualify: {error}')
 
-error, im = VC.process_video(filename)
-
-if np.any(im):
-    image_array_list.append((im, np.asarray(im.shape, np.int32)))
-else:
-    print('This video file does not qualify.')
-
-#%% Run predictions for all models
-
-# Load model from checkpoint
-checkpoint_file = os.path.join(project_dir, 'cfr_a4c_dgx-1_fc1_rest_global_mbf_chkpt_300.h5')
-model = load_model(checkpoint_file)
-model.summary()
-
-# Predict from image_array_list
-n_steps = int(np.ceil(len(image_array_list) / batch_size))
-predictions = predict_from_array_list(model=model,
-                                      model_dict=model_dict,
-                                      feature_dict=feature_dict,
-                                      array_list=image_array_list,
-                                      batch_size=batch_size)
+    return predictions
